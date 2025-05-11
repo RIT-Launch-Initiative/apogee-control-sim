6d7aeba14ebd826588dbabfc884196bc7599c641
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2025-05-11 16:03:26 -0400

    Removed hold and grid config from project startup/shutdown functions

--- project_shutdown.m
+++ project_shutdown.m
@@ -1,11 +1,1 @@
 set(groot, "DefaultFigureWindowStyle", "remove");
-set(groot, "DefaultAxesFontSize", "remove"); 
-set(groot, "DefaultAxesNextPlot", "remove"); % default hold-on
-
-% set all grids default on
-grids = compose("defaultAxes%sGrid", ["X", "Y", "Z"]);
-for grid = grids
-    set(groot, grid, "remove");
-end
-
-clear grids;
