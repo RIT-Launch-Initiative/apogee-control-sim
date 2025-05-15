8e553babf46bd554d2e6b308c2b1a67eb0a9c10a
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2025-05-15 13:12:45 -0400

    3x speedup replacing fzero with linear search

--- sim/find_extension.m
+++ sim/find_extension.m
@@ -1,22 +1,37 @@
 function ext = find_extension(target, simin, guess)
     % set up optimization
-    simin = simin.setVariable(control_mode = "const");
-    opts = optimset(Display = "none", TolX = 1e-3); 
+    tol = 1; % [m]
+    iters_left = 100;
+    err = Inf;
     
     % execute optimization
-    if errfunc(0) <= 0 % no point running if we can't hit apogee target as-is
+    si = simin.setVariable(const_brake = 0);
+    so = sim(si);
+    upper_bound = so.apogee;
+    if upper_bound <= target
         ext = 0;
-    elseif errfunc(1) >= 0 % no point running if we can't hit apogee target as-is
-        ext = 1;
-    else
-        % a bit faster than fminsearch since this problem is 1D
-        ext = fzero(@errfunc, guess, opts);
+        return;
     end
+    si = simin.setVariable(const_brake = 1);
+    so = sim(si);
+    lower_bound = so.apogee;
+    if lower_bound >= target
+        ext = 1;
+        return;
+    end
+    
+    rang = upper_bound - lower_bound;
+    ext = (target - lower_bound) / rang;
+    while (abs(err) > tol) && (iters_left > 0)
+        iters_left = iters_left - 1;
 
-    function [error] = errfunc(ext)
-        ext = clip(ext, 0, 1);
         si = simin.setVariable(const_brake = ext);
         so = sim(si);
-        error = so.apogee - target;
+        err = so.apogee - target;
+        ext = ext + err / rang;
     end
+    if iters_left == 0
+        warning("Iteration failed to converge");
+    end
+
 end
